ace5a7387ca78b85d5a81b7ab6b71e668836a6b2
linjson <linjson@163.com>
2017-07-03 09:12:26 +0800

    no message

---
+++ MPChartExample/src/com/xxmassdeveloper/mpchartexample/XPieChartActivity.java
@@ -0,0 +1,299 @@
+
+package com.xxmassdeveloper.mpchartexample;
+
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.interfaces.datasets.IDataSet;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.sharechart.xpie.IXPieDataSet;
+import com.github.mikephil.charting.sharechart.xpie.XPieChart;
+import com.github.mikephil.charting.sharechart.xpie.XPieData;
+import com.github.mikephil.charting.sharechart.xpie.XPieDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.xxmassdeveloper.mpchartexample.notimportant.DemoBase;
+
+import java.util.ArrayList;
+
+public class XPieChartActivity extends DemoBase implements OnSeekBarChangeListener,
+        OnChartValueSelectedListener {
+
+    private XPieChart mChart;
+    private SeekBar mSeekBarX, mSeekBarY;
+    private TextView tvX, tvY;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_xpiechart);
+
+        tvX = (TextView) findViewById(R.id.tvXMax);
+        tvY = (TextView) findViewById(R.id.tvYMax);
+
+        mSeekBarX = (SeekBar) findViewById(R.id.seekBar1);
+        mSeekBarY = (SeekBar) findViewById(R.id.seekBar2);
+        mSeekBarX.setProgress(4);
+        mSeekBarY.setProgress(10);
+
+        mChart = (XPieChart) findViewById(R.id.chart1);
+        mChart.setUsePercentValues(true);
+        mChart.getDescription().setEnabled(false);
+        mChart.setExtraOffsets(5, 10, 5, 5);
+
+        mChart.setDragDecelerationFrictionCoef(0.95f);
+
+        mChart.setCenterTextTypeface(mTfLight);
+        mChart.setCenterText(generateCenterSpannableText());
+
+        mChart.setPieSpace(5f);
+//        mChart.setDrawHoleEnabled(true);
+//        mChart.setHoleColor(Color.WHITE);
+//
+//        mChart.setTransparentCircleColor(Color.WHITE);
+//        mChart.setTransparentCircleAlpha(110);
+//
+//        mChart.setHoleRadius(58f);
+//        mChart.setTransparentCircleRadius(61f);
+
+        mChart.setDrawCenterText(false);
+
+        mChart.setRotationAngle(0);
+        // enable rotation of the chart by touch
+        mChart.setRotationEnabled(true);
+        mChart.setHighlightPerTapEnabled(true);
+
+//         mChart.setUnit(" €");
+        // mChart.setDrawUnitsInChart(true);
+
+        // add a selection listener
+        mChart.setOnChartValueSelectedListener(this);
+
+        setData(4, 100);
+
+//        mChart.animateY(1400, Easing.EasingOption.EaseInOutQuad);
+        // mChart.spin(2000, 0, 360);
+
+        mSeekBarX.setOnSeekBarChangeListener(this);
+        mSeekBarY.setOnSeekBarChangeListener(this);
+
+        Legend l = mChart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
+        l.setOrientation(Legend.LegendOrientation.VERTICAL);
+        l.setDrawInside(false);
+        l.setXEntrySpace(7f);
+        l.setYEntrySpace(0f);
+        l.setYOffset(0f);
+
+        // entry label styling
+//        mChart.setEntryLabelColor(Color.BLACK);
+        mChart.setEntryLabelTypeface(mTfRegular);
+        mChart.setEntryLabelTextSize(12f);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.pie, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.actionToggleValues: {
+                for (IDataSet<?> set : mChart.getData().getDataSets())
+                    set.setDrawValues(!set.isDrawValuesEnabled());
+
+                mChart.invalidate();
+                break;
+            }
+            case R.id.actionToggleHole: {
+//                if (mChart.isDrawHoleEnabled())
+//                    mChart.setDrawHoleEnabled(false);
+//                else
+//                    mChart.setDrawHoleEnabled(true);
+//                mChart.invalidate();
+//                break;
+            }
+            case R.id.actionDrawCenter: {
+                if (mChart.isDrawCenterTextEnabled())
+                    mChart.setDrawCenterText(false);
+                else
+                    mChart.setDrawCenterText(true);
+                mChart.invalidate();
+                break;
+            }
+            case R.id.actionToggleXVals: {
+
+//                mChart.setDrawEntryLabels(!mChart.isDrawEntryLabelsEnabled());
+                mChart.invalidate();
+                break;
+            }
+            case R.id.actionSave: {
+                // mChart.saveToGallery("title"+System.currentTimeMillis());
+                mChart.saveToPath("title" + System.currentTimeMillis(), "");
+                break;
+            }
+            case R.id.actionTogglePercent:
+                mChart.setUsePercentValues(!mChart.isUsePercentValuesEnabled());
+                mChart.invalidate();
+                break;
+            case R.id.animateX: {
+                mChart.animateX(1400);
+                break;
+            }
+            case R.id.animateY: {
+                mChart.animateY(1400);
+                break;
+            }
+            case R.id.animateXY: {
+                mChart.animateXY(1400, 1400);
+                break;
+            }
+            case R.id.actionToggleSpin: {
+                mChart.spin(1000, mChart.getRotationAngle(), mChart.getRotationAngle() + 360, Easing.EasingOption
+                        .EaseInCubic);
+                break;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+        tvX.setText("" + (mSeekBarX.getProgress()));
+        tvY.setText("" + (mSeekBarY.getProgress()));
+        mChart.setRotationAngle(mSeekBarX.getProgress() % 360);
+        mChart.invalidate();
+//        setData(mSeekBarX.getProgress(), mSeekBarY.getProgress());
+    }
+
+    private void setData(int count, float range) {
+
+        float mult = range;
+        ArrayList<IXPieDataSet> list = new ArrayList<>();
+
+        ArrayList<PieEntry> x = new ArrayList<>();
+        x.add(new PieEntry(20, "A"));
+        x.add(new PieEntry(30, "B"));
+        x.add(new PieEntry(50, "C"));
+
+
+        XPieDataSet dataSet = new XPieDataSet(x, null);
+
+        x = new ArrayList<>();
+
+        x.add(new PieEntry(50, "D"));
+        x.add(new PieEntry(50, "E"));
+        XPieDataSet dataSet2 = new XPieDataSet(x, null);
+        //dataSet.setSelectionShift(0f);
+
+        dataSet.setColors(ColorTemplate.VORDIPLOM_COLORS);
+        dataSet2.setColors(ColorTemplate.JOYFUL_COLORS);
+        config(dataSet);
+        config(dataSet2);
+
+
+        dataSet2.setDrawHoleEnabled(false);
+        list.add(dataSet);
+        list.add(dataSet2);
+
+
+        XPieData data = new XPieData(list);
+        data.setValueFormatter(new PercentFormatter());
+        data.setValueTextSize(11f);
+
+        data.setValueTextColor(Color.BLACK);
+        data.setValueTypeface(mTfLight);
+        mChart.setData(data);
+
+        // undo all highlights
+        mChart.highlightValues(null);
+
+        mChart.invalidate();
+    }
+
+
+    private void config(XPieDataSet dataSet) {
+        dataSet.setXValuePosition(PieDataSet.ValuePosition.OUTSIDE_SLICE);
+        dataSet.setYValuePosition(PieDataSet.ValuePosition.OUTSIDE_SLICE);
+        dataSet.setValueLineColor(Color.RED);
+        dataSet.setValueLinePart1Length(0.6f);
+        dataSet.setValueLinePart2Length(0.4f);
+        dataSet.setValueLinePart1OffsetPercentage(60f);
+
+        dataSet.setSliceSpace(3f);
+        dataSet.setSelectionShift(15f);
+
+        dataSet.setDrawHoleEnabled(true);
+        dataSet.setHoleColor(Color.WHITE);
+
+        dataSet.setTransparentCircleColor(Color.WHITE);
+        dataSet.setTransparentCircleAlpha(110);
+
+        dataSet.setHoleRadius(70f);
+        dataSet.setTransparentCircleRadius(61f);
+    }
+
+    private SpannableString generateCenterSpannableText() {
+
+        SpannableString s = new SpannableString("MPAndroidChart\ndeveloped by Philipp Jahoda");
+        s.setSpan(new RelativeSizeSpan(1.7f), 0, 14, 0);
+        s.setSpan(new StyleSpan(Typeface.NORMAL), 14, s.length() - 15, 0);
+        s.setSpan(new ForegroundColorSpan(Color.GRAY), 14, s.length() - 15, 0);
+        s.setSpan(new RelativeSizeSpan(.8f), 14, s.length() - 15, 0);
+        s.setSpan(new StyleSpan(Typeface.ITALIC), s.length() - 14, s.length(), 0);
+        s.setSpan(new ForegroundColorSpan(ColorTemplate.getHoloBlue()), s.length() - 14, s.length(), 0);
+        return s;
+    }
+
+    @Override
+    public void onValueSelected(Entry e, Highlight h) {
+
+        if (e == null)
+            return;
+        Log.i("VAL SELECTED",
+                "Value: " + e.getY() + ", index: " + h.getX()
+                        + ", DataSet index: " + h.getDataSetIndex());
+    }
+
+    @Override
+    public void onNothingSelected() {
+        Log.i("PieChart", "nothing selected");
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        // TODO Auto-generated method stub
+
+    }
+}
